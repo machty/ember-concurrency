0f775d0cea33f6525b3875db69f8830859c83b8d
machty <machty@gmail.com>
2016-01-08 20:18:41 -0500

    Add test for regular function loopers

--- tests/unit/looper-test.js
+++ tests/unit/looper-test.js
@@ -3,36 +3,48 @@ import { csp, channel, looper } from 'ember-processes';
 
 module('Unit: Loopers');
 
-test('loopers take a channel name', function(assert) {
-  QUnit.stop();
-  assert.expect(3);
+function testLooper(testName, makeHandler) {
+  test('loopers take a channel name (' + testName + ')', function(assert) {
+    QUnit.stop();
+    assert.expect(3);
 
-  let outch = csp.chan();
+    let outch = csp.chan();
 
-  let MyObject = Ember.Object.extend({
-    myChannel: channel(),
-    doStuff: looper('myChannel', function * (value) {
-      yield csp.putAsync(outch, value);
-    }),
+    let MyObject = Ember.Object.extend({
+      myChannel: channel(),
+      doStuff: looper('myChannel', makeHandler(outch)),
+    });
+
+    let obj;
+    Ember.run(() => {
+      obj = MyObject.create();
+      let chan = obj.get('myChannel');
+
+      csp.putAsync(chan, 1);
+      csp.putAsync(chan, 2);
+      csp.putAsync(chan, 3);
+    });
+
+    csp.go(function * () {
+      assert.equal(yield outch, 1);
+      assert.equal(yield outch, 2);
+      assert.equal(yield outch, 3);
+      QUnit.start();
+    });
+
+    Ember.run(obj, 'destroy');
   });
+}
 
-  let obj;
-  Ember.run(() => {
-    obj = MyObject.create();
-    let chan = obj.get('myChannel');
+testLooper("generator", function(outch) {
+  return function * (value) {
+    yield csp.put(outch, value);
+  };
+});
 
-    csp.putAsync(chan, 1);
-    csp.putAsync(chan, 2);
-    csp.putAsync(chan, 3);
-  });
-
-  csp.go(function * () {
-    assert.equal(yield outch, 1);
-    assert.equal(yield outch, 2);
-    assert.equal(yield outch, 3);
-    QUnit.start();
-  });
-
-  Ember.run(obj, 'destroy');
+testLooper("regular function", function(outch) {
+  return function(value) {
+    csp.putAsync(outch, value);
+  };
 });
 
