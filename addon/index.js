64e40c09f71868fa35e6eedfe167412388d472d0
machty <machty@gmail.com>
2016-01-21 17:43:21 -0500

    create asyncIterator.fromEvented

--- addon/index.js
+++ addon/index.js
@@ -282,3 +282,29 @@ export function asyncIterator(obs) {
   });
 }
 
+let EventedObservable = Ember.Object.extend({
+  obj: null,
+  eventName: null,
+
+  subscribe(onNext) {
+    let obj = this.obj;
+    let eventName = this.eventName;
+    obj.on(eventName, onNext);
+
+    let isDisposed = false;
+    return {
+      dispose: () => {
+        if (isDisposed) { return; }
+        isDisposed = true;
+        obj.off(eventName, onNext);
+      }
+    };
+  },
+});
+
+asyncIterator.fromEvent = function(obj, eventName) {
+  return AsyncIterator.create({
+    _observable: EventedObservable.create({ obj, eventName })
+  });
+};
+
