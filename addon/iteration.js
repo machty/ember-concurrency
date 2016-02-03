8d0145ab45c27eec3d0027d0e2d84ec7e01c2fcf
machty <machty@gmail.com>
2016-02-03 10:00:31 -0500

    Iteration steppers and tests

--- addon/iteration.js
+++ addon/iteration.js
@@ -1,25 +1,43 @@
-let NO_VALUE_YET = {};
+const NO_VALUE_YET = {};
+const NEXT   = 'next';
+const THROW  = 'throw';
+const RETURN = 'return';
+const BREAK = 'break';
+const FORCE = {};
 
 function Iteration (iterator, fn) {
   this.iterator = iterator;
   this.fn = fn;
   this.lastValue = NO_VALUE_YET;
+  this.index = 0;
 }
 
-Iteration.prototype.step = function() {
-  let result = this.iterator.next();
-  this.lastValue = result;
+Iteration.prototype.step = function(index) {
+  this._step(NEXT, index);
+};
+
+Iteration.prototype._step = function(iterFn, index) {
+  if (index !== this.index) {
+    return;
+  }
+  this.index++;
+  if (iterFn) {
+    this.lastValue = this.iterator[iterFn]();
+  }
+  this._runFunctionWithIndex();
+};
+
+Iteration.prototype.redo = function(index) {
+  this._step(null, index);
+};
+
+Iteration.prototype.break = function(index) {
+  this._step(RETURN, index);
+};
+
+Iteration.prototype._runFunctionWithIndex = function() {
+  let result = Object.assign({ index: this.index }, this.lastValue);
   this.fn(result);
-};
-
-Iteration.prototype.redo = function() {
-  this.fn(this.lastValue);
-};
-
-Iteration.prototype.break = function() {
-  let result = this.iterator.return();
-  this.lastValue = result;
-  this.fn(this.lastValue);
 };
 
 export function _makeIteration(iterator, fn) {
--- tests/unit/iteration-test.js
+++ tests/unit/iteration-test.js
@@ -19,16 +19,16 @@ test("stepping through a Iteration", function(assert) {
     outerValue = v;
   });
 
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: false, value: 1 });
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: false, value: 2 });
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: false, value: 3 });
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: true, value: undefined });
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: true, value: undefined });
+  Ember.run(iteration, 'step', 0);
+  assert.deepEqual(outerValue, { index: 1, done: false, value: 1 });
+  Ember.run(iteration, 'step', 1);
+  assert.deepEqual(outerValue, { index: 2, done: false, value: 2 });
+  Ember.run(iteration, 'step', 2);
+  assert.deepEqual(outerValue, { index: 3, done: false, value: 3 });
+  Ember.run(iteration, 'step', 3);
+  assert.deepEqual(outerValue, { index: 4, done: true, value: undefined });
+  Ember.run(iteration, 'step', 4);
+  assert.deepEqual(outerValue, { index: 5, done: true, value: undefined });
 });
 
 test("Iterations let you .redo() the same element over and over", function(assert) {
@@ -40,24 +40,24 @@ test("Iterations let you .redo() the same element over and over", function(asser
     outerValue = v;
   });
 
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: false, value: 1 });
-  Ember.run(iteration, 'redo');
-  assert.deepEqual(outerValue, { done: false, value: 1 });
-  Ember.run(iteration, 'redo');
-  assert.deepEqual(outerValue, { done: false, value: 1 });
-  Ember.run(iteration, 'redo');
-  assert.deepEqual(outerValue, { done: false, value: 1 });
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: false, value: 2 });
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: false, value: 3 });
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: true, value: undefined });
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: true, value: undefined });
-  Ember.run(iteration, 'redo');
-  assert.deepEqual(outerValue, { done: true, value: undefined });
+  Ember.run(iteration, 'step', 0);
+  assert.deepEqual(outerValue, { index: 1, done: false, value: 1 });
+  Ember.run(iteration, 'redo', 1);
+  assert.deepEqual(outerValue, { index: 2, done: false, value: 1 });
+  Ember.run(iteration, 'redo', 2);
+  assert.deepEqual(outerValue, { index: 3, done: false, value: 1 });
+  Ember.run(iteration, 'redo', 3);
+  assert.deepEqual(outerValue, { index: 4, done: false, value: 1 });
+  Ember.run(iteration, 'step', 4);
+  assert.deepEqual(outerValue, { index: 5, done: false, value: 2 });
+  Ember.run(iteration, 'step', 5);
+  assert.deepEqual(outerValue, { index: 6, done: false, value: 3 });
+  Ember.run(iteration, 'step', 6);
+  assert.deepEqual(outerValue, { index: 7, done: true, value: undefined });
+  Ember.run(iteration, 'step', 7);
+  assert.deepEqual(outerValue, { index: 8, done: true, value: undefined });
+  Ember.run(iteration, 'redo', 8);
+  assert.deepEqual(outerValue, { index: 9, done: true, value: undefined });
 });
 
 test("Iterations let you .break() out of iteration", function(assert) {
@@ -69,13 +69,42 @@ test("Iterations let you .break() out of iteration", function(assert) {
     outerValue = v;
   });
 
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: false, value: 1 });
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: false, value: 2 });
-  Ember.run(iteration, 'break');
-  assert.deepEqual(outerValue, { done: true, value: undefined });
-  Ember.run(iteration, 'step');
-  assert.deepEqual(outerValue, { done: true, value: undefined });
+  Ember.run(iteration, 'step', 0);
+  assert.deepEqual(outerValue, { index: 1, done: false, value: 1 });
+  Ember.run(iteration, 'step', 1);
+  assert.deepEqual(outerValue, { index: 2, done: false, value: 2 });
+  Ember.run(iteration, 'break', 2);
+  assert.deepEqual(outerValue, { index: 3, done: true, value: undefined });
+  Ember.run(iteration, 'step', 3);
+  assert.deepEqual(outerValue, { index: 4, done: true, value: undefined });
 });
+
+test("Iterations ignore stepping functions if the wrong index is passed in", function(assert) {
+  assert.expect(2);
+
+  let iterator = _makeIteratorFromFunction(oneTwoThree, {}, []);
+  let currentExpectedValue;
+  let iteration = _makeIteration(iterator, v => {
+    if (!currentExpectedValue) {
+      assert.ok(false, "step function shouldn't have run");
+    } else {
+      assert.deepEqual(v, currentExpectedValue);
+    }
+  });
+
+  let runAndExpect = (stepFnName, index, expectedValue) => {
+    currentExpectedValue = expectedValue;
+    Ember.run(iteration, stepFnName, index);
+  };
+
+  runAndExpect('step',  1, false);
+  runAndExpect('step', -1, false);
+  runAndExpect('step', 0, { index: 1, done: false, value: 1 });
+  runAndExpect('step',  -2, false);
+  runAndExpect('step', 0, false);
+  runAndExpect('step', 1, { index: 2, done: false, value: 2 });
+  runAndExpect('break', 0, false);
+  runAndExpect('redo', 0, false);
+});
+
 
