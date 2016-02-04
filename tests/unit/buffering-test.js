a31a80838b0a7ebc5ef27d2fdebac8995faa3802
machty <machty@gmail.com>
2016-02-04 09:43:21 -0500

    refactor buffering test into something reusable

--- tests/unit/buffering-test.js
+++ tests/unit/buffering-test.js
@@ -11,65 +11,48 @@ const {
 
 module('Unit: buffering');
 
-test("async forEach: dropIntermediateValues", function(assert) {
-  QUnit.stop();
-  assert.expect(1);
+// emits 1-10 right away, then 101-110 150ms later
+let rangeObservable = Observable.concat(
+  range(1,10),
+  range(101,10).delay(150)
+);
 
-  // emits 1-10 right away, then 101-110 150ms later
-  let observable = Observable.concat(
-    range(1,10),
-    range(101,10).delay(150)
-  );
+// 1 (5ms) 2 3 (100ms) 4 5 6
+let sporadicObservable = Observable.concat(
+  just(1),
+  just(2).delay(5),
+  just(3),
+  just(4).delay(100),
+  just(5),
+  just(6)
+);
 
-  let arr = [];
-  Ember.run(() => {
-    let obj = Ember.Object.create();
-    forEach(observable, function * (v) {
-      dropIntermediateValues();
+function doBufferingTest(description, observable, bufferPolicyFn, expectations) {
+  test(description, function(assert) {
+    QUnit.stop();
+    assert.expect(1);
 
-      arr.push(v);
-      yield interval(10);
-      if (v === 101) {
-        Ember.run.later(() => {
-          QUnit.start();
-          assert.deepEqual(arr, [1,101]);
-        }, 20);
-      }
-    }).attach(obj);
+    let last = expectations[expectations.length-1];
+
+    let arr = [];
+    Ember.run(() => {
+      let obj = Ember.Object.create();
+      forEach(observable, function * (v) {
+        bufferPolicyFn();
+
+        arr.push(v);
+        yield interval(10);
+        if (v === last) {
+          Ember.run.later(() => {
+            QUnit.start();
+            assert.deepEqual(arr, expectations);
+          }, 20);
+        }
+      }).attach(obj);
+    });
   });
-});
+}
 
-test("async forEach: dropIntermediateValues", function(assert) {
-  QUnit.stop();
-  assert.expect(1);
-
-  // 1 (5ms) 2 3 (100ms) 4 5 6
-  let observable = Observable.concat(
-    just(1),
-    just(2).delay(5),
-    just(3),
-    just(4).delay(100),
-    just(5),
-    just(6)
-  );
-
-  let arr = [];
-  Ember.run(() => {
-    let obj = Ember.Object.create();
-    forEach(observable, function * (v) {
-      dropIntermediateValues();
-
-      arr.push(v);
-      yield interval(50);
-      if (v === 4) {
-        Ember.run.later(() => {
-          QUnit.start();
-          assert.deepEqual(arr, [1,4]);
-        }, 20);
-      }
-    }).attach(obj);
-  });
-});
-
-
+doBufferingTest("dropIntermediateValues: ranges", rangeObservable, dropIntermediateValues, [1,101]);
+doBufferingTest("dropIntermediateValues: sporadic", sporadicObservable, dropIntermediateValues, [1,4]);
 
