d708b8c57a80edb5d84597574eff65825b8ce424
machty <machty@gmail.com>
2016-02-11 13:35:37 -0500

    docs: clean up press-and-hold exapmle

--- tests/dummy/app/components/press-and-hold-button/component.js
+++ tests/dummy/app/components/press-and-hold-button/component.js
@@ -1,22 +1,22 @@
 import Ember from 'ember';
 
 // BEGIN-SNIPPET increment-button
-function increment() {
+function sendPress() {
   this.sendAction('press');
 }
 
-function stopIncrementing() {
+function sendRelease() {
   this.sendAction('release');
 }
 
 export default Ember.Component.extend({
   tagName: 'button',
 
-  touchStart: increment,
-  mouseDown:  increment,
-  touchEnd:   stopIncrementing,
-  mouseLeave: stopIncrementing,
-  mouseUp:    stopIncrementing,
+  touchStart: sendPress,
+  mouseDown:  sendPress,
+  touchEnd:   sendRelease,
+  mouseLeave: sendRelease,
+  mouseUp:    sendRelease,
 });
 // END-SNIPPET
 
