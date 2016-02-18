d532e8af59aa9c778c6afc88061782965f873e18
Robert Jackson <robert.w.jackson@me.com>
2016-02-17 20:47:42 -0500

    Automatically include the polyfill in the host app.

--- index.js
+++ index.js
@@ -2,5 +2,31 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-concurrency'
+  name: 'ember-concurrency',
+
+  included: function() {
+    this._super.included.apply(this, arguments);
+
+    this._includePolyfill();
+  },
+
+  /*
+   Find the main including host app. We are using
+   this to ensure that `includePolyfill` is only
+   done once at the top level (we don't want to
+   include the polyfill more than once)
+   */
+  _includePolyfill: function() {
+    var hostApp = this._findApp();
+    hostApp.options.babel.includePolyfill = true;
+  },
+
+  _findApp: function() {
+    var app = this.app;
+    while (app.app) {
+      app = app.app;
+    }
+
+    return app;
+  }
 };
