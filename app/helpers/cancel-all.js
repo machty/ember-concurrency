7ea08b54943c9e43e5f4dfdc3da8468b845fb6dd
machty <machty@gmail.com>
2016-02-23 09:59:25 -0500

    rename cancel helper to cancel-all for consistency

--- app/helpers/cancel-all.js
+++ app/helpers/cancel-all.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 import { taskHelperClosure } from 'ember-concurrency/-helpers';
 
 export function cancelHelper(args) {
-  return taskHelperClosure('cancel', 'cancelAll', args);
+  return taskHelperClosure('cancel-all', 'cancelAll', args);
 }
 
 export default Ember.Helper.helper(cancelHelper);
