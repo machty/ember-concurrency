32cae21596ac9ba93112ba4c9bb19c66432fd760
machty <machty@gmail.com>
2016-02-26 02:39:05 -0500

    Simplify accelerating buttons example
    @lukemelia suggested this during the Ember NYC
    presenation :)

--- tests/dummy/app/docs/examples/increment-buttons/controller.js
+++ tests/dummy/app/docs/examples/increment-buttons/controller.js
@@ -5,15 +5,13 @@ import { task, timeout } from 'ember-concurrency';
 export default Ember.Controller.extend({
   count: 0,
   incrementBy: task(function * (inc) {
-    if (!inc) { return; }
-
     let speed = 400;
     while (true) {
       this.incrementProperty('count', inc);
       yield timeout(speed);
       speed = Math.max(50, speed * 0.8);
     }
-  }).restartable(),
+  })
 });
 // END-SNIPPET
 
