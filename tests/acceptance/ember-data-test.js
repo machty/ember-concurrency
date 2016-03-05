bd610b2aa130d8d1d9f7c99a4ad3e1f3eb11b927
Jerad Gallinger <jerad@jeradgallinger.ca>
2016-03-05 18:57:34 -0500

    Replicate issue 26 with Pretender

--- tests/acceptance/ember-data-test.js
+++ tests/acceptance/ember-data-test.js
@@ -1,30 +1,51 @@
 import Ember from 'ember';
 import { test } from 'qunit';
 import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
-import { timeout } from 'ember-concurrency';
+
+import Pretender from 'pretender';
 
 moduleForAcceptance('Acceptance | ember-data integration');
 
 test('perform and cancel-all', function(assert) {
   assert.expect(4);
+
+  const pretender = new Pretender(function(){
+    function usersHandler(/* request */){
+      assert.equal(find(buttonSel).text().trim(), 'Request is pending');
+
+      const users = JSON.stringify({
+        data: [
+          {
+            type: "users",
+            id: 1,
+            attributes: { username: "machty" }
+          }, {
+            type: "users",
+            id: 2,
+            attributes: { username: "snoop" },
+          },
+        ]
+      });
+
+      return [200, { "Content-Type": "application/json" }, users];
+    }
+
+    this.get('/users', usersHandler);
+  });
+
   visit('/data-test');
 
   let buttonSel = `[data-test-selector="load-data-button"]`;
 
-    let $button;
   andThen(() => {
-    $button = Ember.$(buttonSel);
-    assert.equal(Ember.$.trim($button.text()), 'Load');
-    click($button);
+    assert.equal(find(buttonSel).text().trim(), 'No request pending');
+    click(buttonSel);
+  });
 
-    return timeout(30).then(() => {
-      assert.equal(Ember.$.trim($button.text()), 'Loading');
-    });
-  }).then(() => {
-    return andThen(() => {
-      assert.equal(Ember.$.trim($button.text()), 'Load');
-      assert.equal(Ember.$(`[data-test-selector="loaded-users"]`).text().replace(/\s/g, ''), "machtysnoop");
-    });
+  andThen(() => {
+    assert.equal(find(buttonSel).text().trim(), 'No request pending');
+    assert.equal(Ember.$(`[data-test-selector="loaded-users"]`).text().trim().replace(/\s+/g, ' '), "machty snoop");
+
+    pretender.shutdown();
   });
 });
-
