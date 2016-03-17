72f70b6c327f5242ca623d61ea0595b5f9093896
machty <machty@gmail.com>
2016-03-17 17:01:46 -0400

    Get rid of homepage, redirect to docs

--- tests/acceptance/root-test.js
+++ tests/acceptance/root-test.js
@@ -1,19 +1,9 @@
-import Ember from 'ember';
 import { test } from 'qunit';
 import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
 
 import DocsController from 'dummy/docs/controller';
 
 moduleForAcceptance('Acceptance | root');
-
-test('visiting /', function(assert) {
-  visit('/');
-
-  andThen(function() {
-    assert.equal(currentURL(), '/');
-    assert.ok(Ember.$('.navbar h3').text(), 'ember-concurrency');
-  });
-});
 
 DocsController.proto().get('flatContents').forEach(page => {
   test(`visiting ${page.route}`, function(assert) {
