374063e1e05efea09fdd980d7a3015ecb0975b55
machty <machty@gmail.com>
2016-06-03 01:40:53 -0400

    prevent perform and cancel-all autoruns

--- addon/-helpers.js
+++ addon/-helpers.js
@@ -3,14 +3,14 @@ export function taskHelperClosure(taskMethod, _args, hash) {
   let task = _args[0];
   let outerArgs = _args.slice(1);
 
-  return (...innerArgs) => {
+  return Ember.run.bind(null, function(...innerArgs) {
     if (hash && hash.value) {
       let event = innerArgs.pop();
       innerArgs.push(Ember.get(event, hash.value));
     }
 
     return task[taskMethod](...outerArgs, ...innerArgs);
-  };
+  });
 }
 
 
