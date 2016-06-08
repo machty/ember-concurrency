f094b06b1b01047889b2a5c452464c72b0f3fa01
machty <machty@gmail.com>
2016-06-08 10:46:15 -0400

    remove attempt to auto-polyfill includePolyfill
    1. it doesn't seem to consistently work
    2. fixes #57
    3. we shouldn't force the entire polyfill; should
       be possible to just supply regeneratorRuntime

--- index.js
+++ index.js
@@ -2,31 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-concurrency',
-
-  included: function(app) {
-    this._super.included.apply(this, arguments);
-
-    this._includePolyfill(app);
-  },
-
-  /*
-   Find the main including host app. We are using
-   this to ensure that `includePolyfill` is only
-   done once at the top level (we don't want to
-   include the polyfill more than once)
-   */
-  _includePolyfill: function(app) {
-    var hostApp = this._findApp(app);
-    hostApp.options.babel.includePolyfill = true;
-  },
-
-  _findApp: function(hostApp) {
-    var app = this.app || hostApp;
-    while (app.app) {
-      app = app.app;
-    }
-
-    return app;
-  }
+  name: 'ember-concurrency'
 };
