cc6e0d55d05873ada825c797c02f494d4453ae2f
machty <machty@gmail.com>
2016-10-11 09:38:28 -0400

    travis: handle old embers without a routing service

--- tests/dummy/app/components/github-edit/component.js
+++ tests/dummy/app/components/github-edit/component.js
@@ -7,7 +7,12 @@ export default Ember.Component.extend({
 
   routing: Ember.inject.service('-routing'),
   href: Ember.computed('routing.currentPath', function() {
-    let path = this.get('routing.currentPath').replace(/\./g, '/');
+    let path = this.get('routing.currentPath');
+    if (!path) {
+      // `routing` doesn't exist for old ember versions via ember-try
+      return;
+    }
+    path = path.replace(/\./g, '/');
     return `https://github.com/machty/ember-concurrency/edit/master/tests/dummy/app/${path}/template.hbs`;
   }),
   title: "Edit on Github",
