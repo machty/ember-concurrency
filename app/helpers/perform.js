fae8b8157ceb1340e12e266faba733490bf74335
machty <machty@gmail.com>
2016-12-18 18:28:16 -0500

    (perform)ing a non-Task only errors when helper is invoked
    Previously, simply rendering a template with a
    (perform nullRef) would throw an error. Now it
    waits until actually performing the task.
    
    This makes for a nicer debugging experience that's
    more consistent with how the `action` helper(s)
    work.

--- app/helpers/perform.js
+++ app/helpers/perform.js
@@ -1,17 +1,9 @@
 import Ember from 'ember';
-import { Task } from 'ember-concurrency/-task-property';
-
 import { taskHelperClosure } from 'ember-concurrency/-helpers';
 
 export function performHelper(args, hash) {
-  let task = args[0];
-  if (!(task instanceof Task)) {
-    Ember.assert(`The first argument passed to the \`perform\` helper should be a Task object (without quotes); you passed ${task}`, false);
-  }
-
   return taskHelperClosure('perform', args, hash);
 }
 
 export default Ember.Helper.helper(performHelper);
-
 
