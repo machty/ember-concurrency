db4c06bb3362ba8da8e11e51ed4816036e92adea
Ben Demboski <ben@joltlabs.com>
2016-12-24 21:28:47 -0800

    Implement withTestWaiter() decorator
    The withTestWaiter() decorator instructs a task to register a test waiting that checks if it's idle, so that asynchronous helpers in tests will wait for the task to complete.

--- tests/unit/task-test.js
+++ tests/unit/task-test.js
@@ -4,6 +4,16 @@ import { module, test } from 'qunit';
 
 module('Unit: task');
 
+function checkWaiters() {
+  // pre-2.8 the Ember.Test.checkWaiters() API didn't exist, but the
+  // Ember.test.waiters intimate API did.
+  if (Ember.Test.checkWaiters) {
+    return Ember.Test.checkWaiters();
+  } else {
+    return !!Ember.A(Ember.Test.waiters).find(([context, fn]) => !fn.call(context));
+  }
+}
+
 test("task init", function(assert) {
   assert.expect(3);
 
@@ -121,6 +131,155 @@ test("task().cancelOn", function(assert) {
   Ember.run(() => {
     Obj.create().trigger('foo');
   });
+});
+
+test("no test waiter is registered without withTestWaiter() decorator", function(assert) {
+  let Obj = Ember.Object.extend({
+    doStuff: task(function * () {
+      return new Ember.RSVP.Promise((resolve) => null);
+    }),
+  });
+
+  let obj;
+
+  Ember.run(() => {
+    obj = Obj.create();
+    obj.get('doStuff').perform();
+  });
+
+  assert.notOk(checkWaiters());
+});
+
+test("withTestWaiter() decorator works", function(assert) {
+  let Obj = Ember.Object.extend({
+    doStuff: task(function * () {
+      return new Ember.RSVP.Promise((resolve) => {
+        this.resolvePromise = resolve;
+      });
+    }).withTestWaiter(),
+  });
+
+  let obj;
+
+  Ember.run(() => {
+    obj = Obj.create();
+    obj.get('doStuff').perform();
+  });
+
+  assert.ok(checkWaiters(), "waiters are not settled while task is running");
+
+  Ember.run(() => {
+    obj.resolvePromise();
+  });
+
+  assert.notOk(checkWaiters(), "waiters are settled after task completes");
+});
+
+test("withTestWaiter() decorator works with concurrent tasks", function(assert) {
+  let Obj = Ember.Object.extend({
+    doStuff: task(function * () {
+      return new Ember.RSVP.Promise((resolve) => {
+        this.resolvePromises = this.resolvePromises || [];
+        this.resolvePromises.push(resolve);
+      });
+    }).withTestWaiter(),
+  });
+
+  let obj;
+
+  Ember.run(() => {
+    obj = Obj.create();
+    obj.get('doStuff').perform();
+  });
+
+  assert.ok(checkWaiters(), "waiters are not settled while task is running");
+
+  Ember.run(() => {
+    obj.get('doStuff').perform();
+  });
+
+  assert.ok(checkWaiters(), "waiters are not settled while two concurrent tasks are running");
+
+  Ember.run(() => {
+    obj.resolvePromises[0]();
+  });
+
+  assert.ok(checkWaiters(), "waiters are not settled after only one task completes");
+
+  Ember.run(() => {
+    obj.resolvePromises[1]();
+  });
+
+  assert.notOk(checkWaiters(), "waiters are settled after both tasks complete");
+});
+
+test("withTestWaiter() decorator works with restartable()", function(assert) {
+  let Obj = Ember.Object.extend({
+    doStuff: task(function * () {
+      return new Ember.RSVP.Promise((resolve) => {
+        this.resolvePromise = resolve;
+      });
+    }).restartable().withTestWaiter(),
+  });
+
+  let obj;
+
+  Ember.run(() => {
+    obj = Obj.create();
+    obj.get('doStuff').perform();
+  });
+
+  assert.ok(checkWaiters(), "waiters are not settled while task is running");
+
+  Ember.run(() => {
+    obj.get('doStuff').perform();
+  });
+
+  assert.ok(checkWaiters(), "waiters are not settled after restarting task");
+
+  Ember.run(() => {
+    obj.resolvePromise();
+  });
+
+  assert.notOk(checkWaiters(), "waiters are settled after task completes");
+});
+
+test("withTestWaiter() decorator works with enqueue()", function(assert) {
+  let Obj = Ember.Object.extend({
+    doStuff: task(function * () {
+      return new Ember.RSVP.Promise((resolve) => {
+        this.resolvePromises = this.resolvePromises || [];
+        this.resolvePromises.push(resolve);
+      });
+    }).enqueue().withTestWaiter(),
+  });
+
+  let obj;
+
+  Ember.run(() => {
+    obj = Obj.create();
+    obj.get('doStuff').perform();
+  });
+
+  assert.ok(checkWaiters(), "waiters are not settled while task is running");
+
+  Ember.run(() => {
+    obj.get('doStuff').perform();
+  });
+
+  assert.ok(checkWaiters(), "waiters are not settled while task is queued");
+
+  Ember.run(() => {
+    obj.resolvePromises[0]();
+  });
+
+  assert.ok(checkWaiters(), "waiters are not settled after only one task completes");
+
+  Ember.run(() => {
+    obj.resolvePromises[1]();
+  });
+
+  assert.notOk(checkWaiters(), "waiters are settled after both tasks complete");
 });
 
 test(".observes re-performs the task every time the observed property changes in a coalesced manner", function(assert) {
