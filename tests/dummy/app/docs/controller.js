628fe6587a1058a1ec01dbd1d587439f6f153f9a
machty <machty@gmail.com>
2016-12-28 00:34:43 -0500

    replace ember-power-select example with basic DOM

--- tests/dummy/app/docs/controller.js
+++ tests/dummy/app/docs/controller.js
@@ -20,7 +20,6 @@ export default Ember.Controller.extend({
         { route: "docs.error-vs-cancelation", title: "Errors vs. Cancelation (try/catch/finally)"},
       ]
     },
-    //{ route: "docs.lifetime", title: "Lifetime"},
     { route: "docs.child-tasks", title: "Child Tasks"},
     { route: "docs.task-groups", title: "Task Groups"},
     { route: "docs.derived-state", title: "Derived State"},
@@ -28,12 +27,11 @@ export default Ember.Controller.extend({
     { title: "Examples", route: "docs.examples",
       children: [
         {route: "docs.examples.loading-ui", title: "Loading UI"},
-        {route: "docs.examples.autocomplete", title: "Auto-Search & ember-power-select"},
+        {route: "docs.examples.autocomplete", title: "Type-Ahead Search"},
         {route: "docs.examples.increment-buttons", title: "Accelerating Increment Buttons"},
         {route: "docs.examples.ajax-throttling", title: "AJAX Throttling"},
         {route: "docs.examples.route-tasks", title: "Route Tasks"},
         {route: "docs.examples.joining-tasks", title: "Awaiting Multiple Child Tasks"},
-        //{route: "docs.examples.decorating-tasks", title: "Decorating Other Tasks"},
       ]
     },
   ],
