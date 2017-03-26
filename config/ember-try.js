716a8cefa6a4e5f7d750ad72327158b6a91e2b11
machty <machty@gmail.com>
2017-03-25 22:20:43 -0400

    don't try so hard

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,17 +1,47 @@
 /*jshint node:true*/
 module.exports = {
-  useVersionCompatibility: true,
-
   scenarios: [
     {
-      name: 'ember-alpha',
-      allowedToFail: true,
+      name: 'ember-lts-2.4',
       bower: {
-        'dependencies': {
-          'ember': 'alpha'
+        dependencies: {
+          'ember': 'components/ember#lts-2-4'
         },
         resolutions: {
-          'ember': 'alpha'
+          'ember': 'lts-2-4'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.8',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-8'
+        },
+        resolutions: {
+          'ember': 'lts-2-8'
+        }
+      }
+    },
+    {
+      name: 'ember-release',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
+      }
+    },
+    {
+      name: 'ember-beta',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
         }
       }
     }
