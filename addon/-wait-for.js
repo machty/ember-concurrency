b9341d5f02bdecac8b0726ec3bb2df07538bef9d
machty <machty@gmail.com>
2017-03-25 23:57:58 -0400

    use yieldables API for waitForEvent / waitForQueue

---
+++ addon/-wait-for.js
@@ -0,0 +1,47 @@
+import Ember from 'ember';
+
+import { isEventedObject } from './utils';
+
+import {
+  yieldableSymbol,
+  YIELDABLE_CONTINUE
+} from './utils';
+
+class WaitForQueueYieldable {
+  constructor(queueName) {
+    this.queueName = queueName;
+  }
+
+  [yieldableSymbol](taskInstance, resumeIndex) {
+    Ember.run.schedule(this.queueName, () => {
+      taskInstance.proceed(resumeIndex, YIELDABLE_CONTINUE, null);
+    });
+  }
+}
+
+class WaitForEventYieldable {
+  constructor(object, eventName) {
+    this.object = object;
+    this.eventName = eventName;
+  }
+
+  [yieldableSymbol](taskInstance, resumeIndex) {
+    let fn = () => {
+      taskInstance.proceed(resumeIndex, YIELDABLE_CONTINUE, null);
+    };
+    this.object.one(this.eventName, fn);
+    return () => {
+      this.object.off(this.eventName, fn);
+    };
+  }
+}
+
+export function waitForQueue(queueName) {
+  return new WaitForQueueYieldable(queueName);
+}
+
+export function waitForEvent(object, eventName) {
+  Ember.assert(`${object} must include Ember.Evented (or support \`.on()\`, \`.one()\`, and \`.off()\`) to be able to use \`waitForEvent\``, isEventedObject(object));
+  return new WaitForEventYieldable(object, eventName);
+}
+
