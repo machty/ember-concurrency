6bb6760040b2f0e4d1210b4407ee9b93a0d45d90
machty <machty@gmail.com>
2017-05-07 21:23:39 -0400

    acceptance tests; skip undefined routes

--- tests/acceptance/root-test.js
+++ tests/acceptance/root-test.js
@@ -7,6 +7,7 @@ import DocsController from 'dummy/docs/controller';
 moduleForAcceptance('Acceptance | root');
 
 DocsController.proto().get('flatContents').forEach(page => {
+  if (!page.route) { return; }
   test(`visiting ${page.route}`, function * (assert) {
     assert.expect(0);
     let url = page.route.replace(/\./g, '/');
