f9e9d4927b9ceedad0e14656113f271e1cc67e99
cibernox <miguel.camba@gmail.com>
2018-04-27 19:08:02 +0200

    Restore back ie9 support

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -10,7 +10,7 @@ const isCI = !!process.env.CI;
 const isProduction = process.env.EMBER_ENV === 'production';
 
 if (isCI || isProduction) {
-  browsers.push('ie 11');
+  browsers.push('ie 9');
 }
 
 module.exports = {
