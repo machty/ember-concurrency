63901d79f95ae0f34827d8b2870105b5a813c2b6
danwenzel <dan@danwenzel.com>
2019-03-15 13:30:06 -0500

    Use correct task name in snippets

--- tests/dummy/snippets/task-cancelation-example-1.js
+++ tests/dummy/snippets/task-cancelation-example-1.js
@@ -7,7 +7,7 @@ export default Component.extend({
 
   actions: {
     fetchResults() {
-      this.get('doStuff').perform().then((results) => {
+      this.get('queryServer').perform().then((results) => {
         this.set('results', results);
       });
     }
