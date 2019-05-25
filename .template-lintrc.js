8d599d905e777c00bcab82ed8da03aee6b4c3114
Miguel Camba <miguel.camba@gmail.com>
2019-05-26 01:13:31 +0200

    Fix some lint errors in templates

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    "no-inline-styles": false,
+    "quotes": false,
+    "block-indentation": false
+  }
 };
