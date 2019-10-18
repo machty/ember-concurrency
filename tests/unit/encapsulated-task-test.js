24878aebe8a842da0f5bce536296500db289eabe
Max Fierke <max@maxfierke.com>
2019-10-17 21:34:51 -0500

    Add tests for use with non-EmberObject native classes (e.g. GlimmerComponent)

--- tests/unit/encapsulated-task-test.js
+++ tests/unit/encapsulated-task-test.js
@@ -2,6 +2,7 @@ import { run } from '@ember/runloop';
 import RSVP from 'rsvp';
 import EmberObject from '@ember/object';
 import { task } from 'ember-concurrency';
+import { gte } from 'ember-compatibility-helpers';
 import { module, test } from 'qunit';
 
 module('Unit: EncapsulatedTask', function() {
@@ -29,4 +30,32 @@ module('Unit: EncapsulatedTask', function() {
     });
     run(defer, 'resolve');
   });
+
+  if (gte('3.10.0')) {
+    test("encapsulated tasks work with native ES classes and decorators", function(assert) {
+      assert.expect(2);
+
+      let defer;
+
+      class FakeGlimmerComponent {
+        @(task({
+          *perform(...args) {
+            assert.deepEqual(args, [1,2,3]);
+            defer = RSVP.defer();
+            yield defer.promise;
+            return 123;
+          }
+        })) myTask;
+      }
+
+      let obj;
+      run(() => {
+        obj = new FakeGlimmerComponent();
+        obj.myTask.perform(1,2,3).then(v => {
+          assert.equal(v, 123);
+        });
+      });
+      run(defer, 'resolve');
+    });
+  }
 });
