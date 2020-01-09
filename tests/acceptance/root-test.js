c44910a0493947613f46b6f266e68051cb4cc0ef
Max Fierke <max@maxfierke.com>
2020-01-08 21:09:53 -0600

    Cherry-pick less flaky root acceptance test from v2

--- tests/acceptance/root-test.js
+++ tests/acceptance/root-test.js
@@ -1,20 +1,35 @@
-import { timeout } from 'ember-concurrency';
-import { test } from '../../tests/helpers/generator-tests';
-import { module } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-
+import { visit } from '@ember/test-helpers';
+import { run } from '@ember/runloop';
+import { gte } from 'ember-compatibility-helpers';
+import RSVP from 'rsvp';
 import DocsController from 'dummy/docs/controller';
+
+let Promise = RSVP.Promise;
+
+if (gte("3.4.0")) {
+  Promise = window.Promise;
+}
 
 module('Acceptance | root', function(hooks) {
   setupApplicationTest(hooks);
 
   DocsController.proto().get('flatContents').forEach(page => {
     if (!page.route) { return; }
-    test(`visiting ${page.route}`, function * (assert) {
+    let testMethod = page.skipTest ? skip : test;
+    testMethod(`visiting ${page.route}`, async function(assert) {
       assert.expect(0);
       let url = page.route.replace(/\./g, '/');
-      this.visit(url);
-      yield timeout(500);
+
+      visit(url);
+      await new Promise(r => {
+        setTimeout(() => {
+          run.cancelTimers();
+          r();
+        }, 200);
+      });
+      run.cancelTimers();
     });
   });
 });
