b5decf682ced0b1c2739f5b27e62b666ccb8f146
Max Fierke <max@maxfierke.com>
2020-01-15 21:50:27 -0600

    Add test for rerender assertion during task cancelation
    Was occuring in Ember 3.15+ within non-lifeycle or computed contexts
    such as native getters within Glimmer components

--- tests/integration/no-render-breaking-test.js
+++ tests/integration/no-render-breaking-test.js
@@ -44,5 +44,32 @@ module('Integration | no render breaking', function(hooks) {
       `);
       assert.ok(true, "Renders");
     });
+
+    test('Issue #340 | internal task state updates in cancellation do not trigger re-render assertions w/ auto-tracking', async function(assert) {
+      assert.expect(1);
+
+      this.owner.register(
+        'component:e-c-test',
+        Component.extend({
+          layout: hbs`<div>{{this.value}}</div>`,
+
+          get value() {
+            this._super(...arguments);
+
+            this.exampleTask.perform();
+            this.exampleTask.perform();
+
+            return "value";
+          },
+
+          exampleTask: task(function*() {
+            yield timeout(100);
+          }).restartable()
+        })
+      );
+
+      await render(hbs`<ECTest />`);
+      assert.ok(true, "Renders");
+    });
   }
 });
