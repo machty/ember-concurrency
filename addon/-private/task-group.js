51171ff7058ad5e299adc5f6847316001379f1cf
Max Fierke <max@maxfierke.com>
2020-08-19 17:53:02 -0500

    Make sure TaskGroup gets tracked too
    Case Number 483092 of forgetting about task groups

--- addon/-private/task-group.js
+++ addon/-private/task-group.js
@@ -1,6 +1,11 @@
 import { TaskGroup as TaskGroupBase } from "./external/task/task-group";
 import { TASKABLE_MIXIN } from "./taskable-mixin";
+import { TRACKED_INITIAL_TASK_STATE } from "./tracked-state";
 
 export class TaskGroup extends TaskGroupBase { }
+
+if (TRACKED_INITIAL_TASK_STATE) {
+  Object.defineProperties(TaskGroup.prototype, TRACKED_INITIAL_TASK_STATE);
+}
 
 Object.assign(TaskGroup.prototype, TASKABLE_MIXIN);
