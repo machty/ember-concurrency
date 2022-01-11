585f49716c3f8e5bd444a9e9ce9cc5c4de584325
Max Fierke <max@maxfierke.com>
2022-01-10 20:36:03 -0600

    Fix embroider-optimized build part

--- tests/integration/no-render-breaking-test.js
+++ tests/integration/no-render-breaking-test.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { modifier } from 'ember-modifier';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { task, timeout } from 'ember-concurrency';
@@ -13,14 +12,6 @@ module('Integration | no render breaking', function (hooks) {
   if (gte('3.15.0')) {
     test('Issue #337 | internal task state updates do not trigger re-render assertions w/ auto-tracking', async function (assert) {
       assert.expect(1);
-
-      this.owner.register(
-        'modifier:autofocus',
-        modifier(function autofocus(element) {
-          const childElement = element.querySelector('input');
-          childElement.focus();
-        })
-      );
 
       this.owner.register(
         'component:e-c-test',
