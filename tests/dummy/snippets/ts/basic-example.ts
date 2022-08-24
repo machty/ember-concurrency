8ab8debc53685c063326290c98140be6df92023b
machty <machty@gmail.com>
2022-08-24 08:28:00 -0400

    docs: add example of Task type

--- tests/dummy/snippets/ts/basic-example.ts
+++ tests/dummy/snippets/ts/basic-example.ts
@@ -1,21 +1,9 @@
 import Component from '@ember/component';
 import { task, timeout } from 'ember-concurrency';
-import { action } from '@ember/object';
 
 export default class extends Component {
   myTask = task(this, async (ms: number) => {
     await timeout(ms);
     return 'done!';
   });
-
-  @action
-  performTask() {
-    if (this.myTask.isRunning) {
-      return;
-    }
-
-    this.myTask.perform(1000).then(value => {
-      console.log(value.toUpperCase());
-    });
-  }
 }
---
+++ tests/dummy/snippets/ts/typing-task.ts
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component'
+import { task, timeout } from 'ember-concurrency'
+import type { Task } from 'ember-concurrency'
+
+// Define a Type task that takes a single number argument and returns a string
+type MyTaskType = Task<string, [ms: number]>;
+
+interface Args {
+  fooTask: MyTaskType;
+}
+
+export default class extends Component<Args> {
+  slowlyComputeStringLength: MyTaskType = task(this, async (ms: number) => {
+    await timeout(ms);
+
+    const length = await this.args.fooTask.perform(ms);
+
+    return length;
+  });
+}
