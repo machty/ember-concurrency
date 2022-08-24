b6765e9cd3d452f78e6c9d26912ec1d5f6a1a5b7
Alex Matchneer <machty@gmail.com>
2022-08-24 06:50:12 -0400

    ember-try 3.28 (#474)

--- config/ember-try.js
+++ config/ember-try.js
@@ -51,6 +51,14 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
