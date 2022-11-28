22635ac721a4ddbb23ec239898e748cc9b3220cb
Luc Boissaye <luc@boissaye.fr>
2022-11-28 10:04:09 +0100

    Update example with a generic type on a task

--- tests/dummy/snippets/ts/basic-example.ts
+++ tests/dummy/snippets/ts/basic-example.ts
@@ -2,8 +2,14 @@ import Component from '@ember/component';
 import { task, timeout } from 'ember-concurrency';
 
 export default class extends Component {
+  myGenericTask = task(async <H, K extends keyof H>(hash: H, key: K): Promise<H[K]> => {
+    await timeout(100);
+    return hash[key];
+  });
+
   myTask = task(async (ms: number) => {
     await timeout(ms);
-    return 'done!';
+    const res = await this.myGenericTask.perform({a: ' done!'}, 'a');
+    return res;
   });
 }
