faf544c42389b5eb3d5ef80e68607d317981ff59
machty <machty@gmail.com>
2024-02-02 22:10:04 +0800

    rollup build works

--- packages/ember-concurrency/rollup.config.mjs
+++ packages/ember-concurrency/rollup.config.mjs
@@ -1,10 +1,12 @@
-import babel from '@rollup/plugin-babel';
-import { Addon } from '@embroider/addon-dev/rollup';
+import babel from "@rollup/plugin-babel";
+import { Addon } from "@embroider/addon-dev/rollup";
 
 const addon = new Addon({
-  srcDir: 'src',
-  destDir: 'dist',
+  srcDir: "src",
+  destDir: "dist",
 });
+
+// todo: refer to https://github.com/embroider-build/addon-blueprint/blob/b1168b4616adf748bd713c5af39e104325982e9b/files/__addonLocation__/rollup.config.mjs#L4
 
 export default {
   // This provides defaults that work well alongside `publicEntrypoints` below.
@@ -14,25 +16,31 @@ export default {
   plugins: [
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
-    addon.publicEntrypoints(['components/**/*.js', 'services/**/*.js']),
+    addon.publicEntrypoints(["helpers/**/*.js"]),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports(['components/**/*.js', 'services/**/*.js']),
+    addon.appReexports(["helpers/**/*.js"]),
+
+    // Follow the V2 Addon rules about dependencies. Your code can import from
+    // `dependencies` and `peerDependencies` as well as standard Ember-provided
+    // package names.
+    addon.dependencies(),
 
     // This babel config should *not* apply presets or compile away ES modules.
     // It exists only to provide development niceties for you, like automatic
     // template colocation.
-    // See `babel.config.json` for the actual Babel configuration!
-    babel({ babelHelpers: 'bundled' }),
+    //
+    // By default, this will load the actual babel config from the file
+    // babel.config.json.
+    babel({
+      extensions: ['.js', '.gjs', '.ts', '.gts'],
+      babelHelpers: 'bundled',
+    }),
 
     // Ensure that standalone .hbs files are properly integrated as Javascript.
     addon.hbs(),
-
-    // addons are allowed to contain imports of .css files, which we want rollup
-    // to leave alone and keep in the published output.
-    addon.keepAssets(['**/*.css']),
 
     // Remove leftover build artifacts when starting a new build.
     addon.clean(),
