8ba7484469d78b510c3d1e5db5ad3c03ea1e00a8
machty <machty@gmail.com>
2024-02-04 07:27:34 +0800

    rm old code

--- packages/test-app/tests/test-helper.js
+++ packages/test-app/tests/test-helper.js
@@ -1,30 +1,13 @@
 import Application from 'test-app/app';
 import config from 'test-app/config/environment';
 import { setApplication } from '@ember/test-helpers';
-import { QUnitAdapter, start } from 'ember-qunit';
+import { start } from 'ember-qunit';
 import QUnit from 'qunit';
 import { setup as setupQUnitDom } from 'qunit-dom';
 
 setupQUnitDom(QUnit.assert);
 
 QUnit.config.testTimeout = 5000;
-
-function noop() {}
-
-QUnitAdapter.reopen({
-  exception(e) {
-    // our tests depend on Ember.run bubbling errors thrown within the run loop.
-    throw e;
-  },
-
-  asyncStart: function asyncStart() {
-    if (this.qunit.config.current._isTaskTest) {
-      this.doneCallbacks.push({ test: this.qunit.config.current, done: noop });
-    } else {
-      this._super();
-    }
-  },
-});
 
 setApplication(Application.create(config.APP));
 
