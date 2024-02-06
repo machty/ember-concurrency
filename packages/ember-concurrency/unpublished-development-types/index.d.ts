c504d687f1300c6ab715cb80396932d105e2e451
machty <machty@gmail.com>
2024-02-06 10:30:36 +0800

    maybe?

---
+++ packages/ember-concurrency/unpublished-development-types/index.d.ts
@@ -0,0 +1,15 @@
+// Add any types here that you need for local development only.
+// These will *not* be published as part of your addon, so be careful that your published code does not rely on them!
+import 'ember-source/types';
+import 'ember-source/types/preview';
+import '@glint/environment-ember-loose';
+import '@glint/environment-ember-template-imports';
+
+declare module '@glint/environment-ember-loose/registry' {
+  // Remove this once entries have been added! 👇
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export default interface Registry {
+    // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
+    // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
+  }
+}
