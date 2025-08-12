36f60c20da12c94849ebafb627d7e3cada6e9baa
machty <machty@gmail.com>
2025-08-12 12:26:18 -0400

    try 6.4

--- packages/test-app/config/ember-try.js
+++ packages/test-app/config/ember-try.js
@@ -9,22 +9,6 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
-        name: 'ember-lts-3.28',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.4.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
@@ -45,6 +29,14 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~5.0.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-6.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~6.4.0',
           },
         },
       },
