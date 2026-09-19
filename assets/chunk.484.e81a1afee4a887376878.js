7ad8f3be4b44cd781898b1cff3e9936b3906e027
machty <machty@users.noreply.github.com>
2026-09-19 09:27:56 +0000

    deploy: 9968fccb0e4e849836d87eeb8d6903359ea9c645

--- assets/chunk.484.e81a1afee4a887376878.js
+++ assets/chunk.484.e81a1afee4a887376878.js
@@ -1,4 +1,4 @@
-(self.webpackChunk_ember_auto_import_=self.webpackChunk_ember_auto_import_||[]).push([[771],{41:(e,t,s)=>{"use strict"
+(self.webpackChunk_ember_auto_import_=self.webpackChunk_ember_auto_import_||[]).push([[484],{41:(e,t,s)=>{"use strict"
 s.d(t,{O:()=>c,e:()=>o})
 var n=s(473),r=s(536),i=s(587)
 function a(e,t){return Object.keys(e).reduce((t,s)=>function(e,t,s){const r=Object.getOwnPropertyDescriptor(e,s)
@@ -25,7 +25,7 @@ function _(e,t){if(S[e])throw new Error(`A modifier with the name '${e}' has alr
 S[e]=t}function x(e){return S[e]}function E(e){return e in S}let C=class{constructor(e="<unknown>",t=null,s={}){w(this,"env",n.U),w(this,"_debug",null),w(this,"_enabledModifiers",[]),w(this,"_hasSetConcurrencyConstraint",!1),w(this,"_hasSetBufferPolicy",!1),w(this,"_hasEnabledEvents",!1),w(this,"_maxConcurrency",null),w(this,"_onStateCallback",(e,t)=>t.setState(e)),w(this,"_schedulerPolicyClass",b),this.name=e,this.taskDefinition=t,this.options=s,this._processModifierOptions(s)}createTask(e){let t=this.getTaskOptions(e)
 return new v.Y(Object.assign({generatorFactory:t=>this.taskDefinition.apply(e,t)},t))}getModifier(e){if(E(e))return S[e].bind(null,this)}getOptions(){return this.options}getScheduler(e,t){return new k.A(e,t)}getTaskOptions(e){let t,s=this._onStateCallback,n=new this._schedulerPolicyClass(this._maxConcurrency)
 return t=this.getScheduler(n,s&&"function"==typeof s),{context:e,debug:this._debug,env:this.env,name:this.name,group:void 0,scheduler:t,hasEnabledEvents:this._hasEnabledEvents,onStateCallback:s,enabledModifiers:this._enabledModifiers,modifierOptions:this.getOptions()}}setBufferPolicy(e){return function(e){if(e._hasSetBufferPolicy)throw new Error(`Cannot set multiple buffer policies on a task. ${e._schedulerPolicyClass} has already been set for task '${e.name}'`)}(this),this._hasSetBufferPolicy=!0,this._hasSetConcurrencyConstraint=!0,this._schedulerPolicyClass=e,this}setDebug(e){return this._debug=e,this}setEvented(e){return this._hasEnabledEvents=e,this}setMaxConcurrency(e){return this._hasSetConcurrencyConstraint=!0,this._maxConcurrency=e,this}setName(e){return this.name=e,this}setOnState(e){return this._onStateCallback=e,this}setTaskDefinition(e){return this.taskDefinition=e,this}_processModifierOptions(e){if(e)for(let t of Object.keys(e)){let s=e[t],n=this.getModifier(t)
-"function"==typeof n&&n(s)&&this._enabledModifiers.push(t)}}}},96:function(e,t){window._eai_r=require,window._eai_d=define},104:(e,t,s)=>{"use strict"
+"function"==typeof n&&n(s)&&this._enabledModifiers.push(t)}}}},104:(e,t,s)=>{"use strict"
 s.d(t,{w:()=>l})
 var n=s(421),r=s(454),i=s(603),a=s(223),o=s(704)
 class c extends r.O{assert(...e){(0,i.assert)(...e)}async(e){(0,a.join)(()=>(0,a.schedule)("actions",e))}reportUncaughtRejection(e){(0,a.next)(null,function(){const t=(0,o.getOnerror)()
@@ -40,7 +40,7 @@ s.r(t),s.d(t,{cancelHelper:()=>o,default:()=>c})
 var n=s(336),r=s(603),i=s(449)
 const a="the 'cancel-all' template helper was invoked"
 function o(e){let t=e[0]
-return t&&"function"==typeof t.cancelAll||(0,r.assert)(`The first argument passed to the \`cancel-all\` helper should be a Task (without quotes); you passed ${t}`,!1),(0,i.F)("cancel-all","cancelAll",[t,{reason:a}])}var c=(0,n.helper)(o)},378:(e,t,s)=>{"use strict"
+return t&&"function"==typeof t.cancelAll||(0,r.assert)(`The first argument passed to the \`cancel-all\` helper should be a Task (without quotes); you passed ${t}`,!1),(0,i.F)("cancel-all","cancelAll",[t,{reason:a}])}var c=(0,n.helper)(o)},335:function(e,t){window._eai_r=require,window._eai_d=define},378:(e,t,s)=>{"use strict"
 s.d(t,{Jk:()=>o,b5:()=>a,wR:()=>l,y$:()=>u})
 var n=s(223),r=s(104),i=s(581)
 function a(e){return e&&("function"==typeof e.one&&"function"==typeof e.off||"function"==typeof e.on&&"function"==typeof e.off||"function"==typeof e.addEventListener&&"function"==typeof e.removeEventListener)}class o extends i._d{_deferable(){return r.w.defer()}}class c extends o{constructor(e){super(),this.ms=e}onYield(e){let t=(0,n.later)(()=>e.next(),this.ms)
